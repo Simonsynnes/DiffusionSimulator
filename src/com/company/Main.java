d28e1c1f8458ea8aa2acba62342345ed8ee3f5f9
Unknown <olav.telseth94@hotmail.com>
2018-04-13 09:30:05 +0200

    Jonathan sliter med å pushe

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        new CA_DriverV2();
+        //System.out.println("YO");
+
+    }
+}
